a73a34f97b918db195e0a37dd2b8ca1da4506cf8
AuroreMariscal <aurore@affinitic.be>
2019-11-12 11:44:00 +0100

    Add worker to js

--- service.js
+++ service.js
@@ -13,3 +13,28 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", function(){
     //met en écoute le service
 })
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+    workbox.precaching.precacheAndRoute([
+        {
+            "url": "index.html"
+        },
+        {
+            "url": "style.css"
+        },
+        {
+            "url": "manifest.json"
+        },
+        {
+            "url": "https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        },
+        {
+            "url": "icon-96-96.png"
+        },
+    ])
+  } else {
+    console.log(`Boo! Workbox didn't load 😬`);
+  }
